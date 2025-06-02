b2b3e8180a9c77e698fbbae926bb87a8a268c96f
AmirMJvd <amir.m.jvd.1.1@gmail.com>
2025-06-02 11:58:13 +0330

    javat

--- sanjaghak-react/src/pages/signIn.jsx
+++ sanjaghak-react/src/pages/signIn.jsx
@@ -1,19 +1,50 @@
-import React from 'react';
-import { Link ,useNavigate} from 'react-router-dom';
+import React, { use, useRef, useState } from 'react';
+import { data, Link ,useNavigate} from 'react-router-dom';
 import "./signIn.css"
 
 function signIn(){
     const navigate = useNavigate();
+    const [email,setEmail] = useState("");
 
   const handleSubmit = (e) => {
-    navigate('/confirmCode');  
+      e.preventDefault();
+    console.log(email)
+  fetch('http://localhost:8080/api/Sanjaghak/UserAccount/login/requestCode', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email: email,
+      phoneNumber: null
+    })
+  })
+  .then(response => {
+    if (response.ok) {
+      navigate('/confirmCode')
+    } else {
+      return response.text().then(errorMessage => {
+        throw new Error(errorMessage); 
+      });
+    }
+  })
+  .then(data => {
+    console.log("✅ Success:", data);
+  })
+  .catch(error => {
+    console.error("❌ Error:", error.message);
+  });
   };
+  const handleEmailChange =(input)=>{
+    const value=input.target.value;
+    setEmail(value)
+  }
     return(
         <form className='signInBox' onSubmit={handleSubmit}>
             <h1>ورود با ایمیل</h1>
             <p>لطفا ایمیل خود را جهت دریافت کد وارد نمایید</p>
       <div className="inputWrapper">
-        <input type="email" required className="signInInput" id="email" placeholder=" " />
+        <input  type="email" required className="signInInput" id="email" placeholder=" " onChange={handleEmailChange} />
         <label  className="floatingLabel">ایمیل</label>
         <button className="signInButton" type="submit">ارسال کد</button>
       </div>
--- sanjaghak-react/src/pages/signInMobile.jsx
+++ sanjaghak-react/src/pages/signInMobile.jsx
@@ -1,19 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link ,useNavigate} from 'react-router-dom';
 import "./signIn.css"
 
 function signInMobile(){
   const navigate = useNavigate();
+  const [phoneNumber,setPhoneNumber]= useState("")
 
   const handleSubmit = (e) => {
-    navigate('/confirmCode');  
+      e.preventDefault();
+      fetch('http://localhost:8080/api/Sanjaghak/UserAccount/login/requestCode', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      email: null,
+      phoneNumber: phoneNumber
+    })
+  })
+  .then(response => {
+    if (response.ok) {
+      navigate('/confirmCode')
+    } else {
+      return response.text().then(errorMessage => {
+        throw new Error(errorMessage); 
+      });
+    }
+  })
+  .then(data => {
+    console.log("✅ Success:", data);
+  })
+  .catch(error => {
+    console.error("❌ Error:", error.message);
+  });
   };
+  const handleNumberChange=(input)=>{
+    const value=input.target.value;
+    setPhoneNumber(value);
+
+  }
     return(
         <form className='signInBox' onSubmit={handleSubmit}>
             <h1>ورود با شماره موبایل</h1>
             <p>لطفا شماره موبایل خود را جهت دریافت کد وارد نمایید</p>
       <div className="inputWrapper">
-        <input type="tel" required pattern="^09\d{9}$" inputMode="numeric" className="signUpInput"  placeholder=" "/>
+        <input type="tel" required pattern="^09\d{9}$" inputMode="numeric" className="signUpInput"  placeholder=" " onChange={handleNumberChange}/>
         <label  className="floatingLabel">شماره موبایل</label>
           <button className="signInButton" type="submit">ارسال کد</button>
       </div>

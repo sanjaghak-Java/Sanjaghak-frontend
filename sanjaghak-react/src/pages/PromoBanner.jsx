8b3c39d0c9a804671c6e7c9bb40e2038a2d73ca0
artythemighty <amirhossein.hssnpr@gmail.com>
2025-06-04 21:38:46 +0330

    main page components

---
+++ sanjaghak-react/src/pages/PromoBanner.jsx
@@ -0,0 +1,15 @@
+
+import "./promoBanner.css"
+
+function PromoBanner(){
+    return(
+        <div className="PromoContainer">
+            <h1 className="promoTitle">promo title</h1>
+            <h4 className="PromoDescription">promo description</h4>
+            <button className="seeAllBtn">see all</button>
+
+        </div>
+    )
+
+}
+export default PromoBanner

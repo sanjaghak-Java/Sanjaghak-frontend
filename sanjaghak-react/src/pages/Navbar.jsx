cd8770ad957e4a8b623842072a4efc23e8ebe31c
Payam asadi <payam.asadi2006@gmail.com>
2025-06-09 03:08:40 +0330

    تعداد سبد خرید در نوبار

--- sanjaghak-react/src/pages/Navbar.jsx
+++ sanjaghak-react/src/pages/Navbar.jsx
@@ -4,78 +4,81 @@ import { Link, useNavigate } from 'react-router-dom';
 
 function Navbar() {
     const navigate = useNavigate();
-    
-    const goToSignIn=()=>{
+
+    const goToSignIn = () => {
         navigate("/signin")
     }
-    const goToproduct=()=> {
+    const goToproduct = () => {
         navigate("/Product")
     }
-    const goTomycart=()=> {
+    const goTomycart = () => {
         navigate("/mycart")
     };
 
-  return (
-    <div className='navContainer'>
-        <div className='upperNav'>
-            <Link to="/">
-                <img src="./src/assets/sanjaghak-logo.png" alt="" className='logo' />
-            </Link>
-            <div className="searchContainer">
-                <button className="searchBtn">
-                    <img src="./src/assets/search.png" alt="search" />
-                </button>
-                <input type="text" className="searchBar" placeholder="جستجو" />
+    return (
+        <div className='navContainer'>
+            <div className='upperNav'>
+                <Link to="/">
+                    <img src="./src/assets/sanjaghak-logo.png" alt="" className='logo' />
+                </Link>
+                <div className="searchContainer">
+                    <button className="searchBtn">
+                        <img src="./src/assets/search.png" alt="search" />
+                    </button>
+                    <input type="text" className="searchBar" placeholder="جستجو" />
+                </div>
+
+                <div className='userActions'>
+                    <button className='loginBtn' onClick={goToSignIn}>ثبت نام<span className='space'>|</span>ورود</button>
+                    <div className='cart-navbar'>
+                        <button className='cartbutton' onClick={goTomycart}>
+                            <img src="./src/assets/icons8-cart-64.png" alt="cart" className='cartIcon' />
+                            <label htmlFor="" className='cart-num'>1</label>
+                        </button>
+                    </div>
+                </div>
             </div>
 
-            <div className='userActions'>
-                <button className='loginBtn'  onClick={goToSignIn}>ثبت نام<span className='space'>|</span>ورود</button>
-                <button className='cartbutton' onClick={goTomycart}>
-                    <img src="./src/assets/icons8-cart-64.png" alt="cart" className='cartIcon' />
-                </button>
+            <div className='lowerNav'>
+
+                <div className='dropDownContainer'>
+                    <Link className='item'>
+                        <div className='itemContent'>
+                            <img src="./src/assets/justify.png" alt="" className='icon' />
+                            <span className='itemText'>دسته بندی کالا ها</span>
+                        </div>
+                    </Link>
+
+                    <div className='dropDown'>
+                        <Link>a</Link>
+                        <Link>a</Link>
+                        <Link>a</Link>
+                    </div>
+                </div>
+
+                <div className='navItem'>
+                    <Link className='item'>
+                        <div className='itemContent'>
+                            <img src="./src/assets/cup-2.png" alt="" className='icon' />
+                            <span className='itemText'>پرفروش‌ترین‌ها</span>
+                        </div>
+                    </Link>
+                </div>
+
+                <div className='navItem'>
+                    <Link className='item'>
+                        <div className='itemContent'>
+                            <img src="./src/assets/icons8-new-32.png" alt="" className='icon' />
+                            <span className='itemText'>جدید ترین</span>
+                        </div>
+                    </Link>
+                </div>
+
+                <button className='' onClick={goToproduct}>test</button>
             </div>
+
         </div>
-
-        <div className='lowerNav'>
-
-             <div className='dropDownContainer'>
-                 <Link className='item'>
-                 <div className='itemContent'>        
-                     <img src="./src/assets/justify.png" alt="" className='icon' />
-                     <span className='itemText'>دسته بندی کالا ها</span>
-                 </div>
-                 </Link>
-
-                 <div className='dropDown'>
-                 <Link>a</Link>
-                 <Link>a</Link>
-                 <Link>a</Link>
-                 </div>
-             </div>
-
-             <div className='navItem'>
-                 <Link className='item'>
-                 <div className='itemContent'>
-                     <img src="./src/assets/cup-2.png" alt="" className='icon' />
-                     <span className='itemText'>پرفروش‌ترین‌ها</span>
-                 </div>
-                 </Link>
-             </div>
-
-             <div className='navItem'>
-                 <Link className='item'>
-                 <div className='itemContent'>
-                     <img src="./src/assets/icons8-new-32.png" alt="" className='icon' />
-                     <span className='itemText'>جدید ترین</span>
-                 </div>
-                 </Link>
-             </div>
-
-             <button className='' onClick={goToproduct}>test</button>
-        </div>
-
-    </div>
-  );
+    );
 }
 
 export default Navbar;

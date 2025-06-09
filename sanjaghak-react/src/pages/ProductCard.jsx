9c811c693a735990ad476fb392aa02caaf68d3ec
Payam asadi <payam.asadi2006@gmail.com>
2025-06-09 14:30:05 +0330

    بعضی ایشلر

--- sanjaghak-react/src/pages/ProductCard.jsx
+++ sanjaghak-react/src/pages/ProductCard.jsx
@@ -1,35 +1,48 @@
-
+import { useEffect, useState } from "react"
 import "/src/styles/ProductCard.css"
-function ProductCard(props){
-    return(
+
+function ProductCard(props) {
+    const [imageSizeClass, setImageSizeClass] = useState("backImage-small");
+
+    useEffect(() => {
+        const img = new Image();
+        img.src = props.image;
+
+        img.onload = () => {
+            if (img.naturalWidth > 100) {
+                setImageSizeClass("backImage-large");
+            } else {
+                setImageSizeClass("backImage-small");
+            }
+        };
+    }, [props.image]);
+
+    return (
         <div className="cardContainer">
-            <div className="salePercent">{props.salepercent}</div>
-            <img src={props.image} className="productImage"/>
-            <div className='productinfo'>
-                <h2 className="ProductName">{props.name}</h2>
-                <h4 className="ProductModel">{props.model}</h4>
-            </div>
-            <hr />
-            <div className="priceSection">
-                <p>قیمت:</p>
-                <div className="priceDetails">
-                    <h5 className="productSalePrice">{props.salePrice} تومان</h5>
-                    <h2 className="ProductPrice">{props.price} تومان</h2>
+            <div className="cardInner">
+                <div className="cardFront">
+                    <div className="salePercent">{props.salepercent}</div>
+                    <img src={props.image} className="productImage" />
+                    <div className='productinfo'>
+                        <h2 className="ProductName">{props.name}</h2>
+                        <h4 className="ProductModel">{props.model}</h4>
+                    </div>
+                    <hr />
+                    <div className="priceSection">
+                        <p>قیمت:</p>
+                        <div className="priceDetails">
+                            <h5 className="productSalePrice">{props.salePrice} تومان</h5>
+                            <h2 className="ProductPrice">{props.price} تومان</h2>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="cardBack">
+                    <img src={props.image} className={imageSizeClass} />
                 </div>
             </div>
-
-
-
-
-
         </div>
-
-
-
-
-
-
-
     );
 }
+
 export default ProductCard
--- sanjaghak-react/src/pages/categoryBox.jsx
+++ sanjaghak-react/src/pages/categoryBox.jsx
@@ -5,24 +5,24 @@ import "/src/styles/categoryBox.css"
 
 
 
-function CategoryBox(props){
-    return(
-        
+function CategoryBox(props) {
+    return (
+
         <div className="categoryBox">
             <div className="labelsContainer">
-            <p className="categoryTitle">title</p>
-            <div className="labelGroup">
-                <label className="bestsellerLabel" tabIndex="0">bestseller</label>
-                <label className="newestLabel" tabIndex="0">newest</label>
-                <div className="filterWrapper">
-                <img className="filterImg" src="#" />
-                <label className="filterLabel" tabIndex="0">filter</label>
+                <p className="categoryTitle">لپ تاپ</p>
+                <div className="labelGroup">
+                    <label className="bestsellerLabel" tabIndex="0">پرفروش ترین ها</label>
+                    <label className="newestLabel" tabIndex="0">جدید ترین ها</label>
+                    <div className="filterWrapper">
+                        <label className="filterLabel" tabIndex="0">فیلتر</label>
+                        <img className="filterImg" src="/src/assets/icons8-filter-32.png" />
+                    </div>
                 </div>
             </div>
-            </div>
-            <hr/>
-            <br/>
-            <ProductGrid/>
+            <hr />
+            <br />
+            <ProductGrid />
         </div>
 
     );

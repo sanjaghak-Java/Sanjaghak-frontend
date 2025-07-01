b480bdaff62cff1e4db9432e3e721fec71dc371d
Payam asadi <payam.asadi2006@gmail.com>
2025-07-01 13:34:58 +0330

    مجبوری پوش

--- sanjaghak-react/src/pages/categoryBox.jsx
+++ sanjaghak-react/src/pages/categoryBox.jsx
@@ -12,12 +12,13 @@ function CategoryBox(props) {
             <div className="labelsContainer">
                 <p className="categoryTitle">لپ تاپ</p>
                 <div className="labelGroup">
+                    <div className="filterWrapper">
+                        <img className="filterImg" src="/src/assets/icons8-filter-32.png" />
+                        <label className="filterLabel" tabIndex="0">فیلتر</label>
+                    </div>
                     <label className="bestsellerLabel" tabIndex="0">پرفروش ترین ها</label>
                     <label className="newestLabel" tabIndex="0">جدید ترین ها</label>
-                    <div className="filterWrapper">
-                        <label className="filterLabel" tabIndex="0">فیلتر</label>
-                        <img className="filterImg" src="/src/assets/icons8-filter-32.png" />
-                    </div>
+
                 </div>
             </div>
             <br />
